743590aff5b6903dbbec9d62330affd967b3b8a3
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-11-15 16:26:44 -0500

    Création du projet Android

---
+++ CircleDefender/app/src/main/java/ca/qc/cgmatane/informatique/circledefender/VuePrincipale.java
@@ -0,0 +1,13 @@
+package ca.qc.cgmatane.informatique.circledefender;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VuePrincipale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_principale);
+    }
+}

f736153711bc59a657c763fecd309a385b25ee36
Antoine <aherkens@gmail.com>
2018-11-23 00:10:52 -0500

    position des éléments par rapport à un canvas adapté à la taille écran

--- CircleDefender/app/src/main/assets/www/action/vue/JeuVue.js
+++ CircleDefender/app/src/main/assets/www/action/vue/JeuVue.js
@@ -2,10 +2,6 @@ var JeuVue = (function()
 {
     var pageJeuVue = document.getElementById("page-jeu").innerHTML;
 
-    console.log("x is " + window.screen.availHeight);
-    console.log("y is " + window.screen.availWidth);
-    // It works
-
     return function()
     {
         var stagePrincipal;
@@ -14,54 +10,34 @@ var JeuVue = (function()
 
 
         var initialiser = function(){
+
+            var canvas = document.createElement('canvas');
+            canvas.id = "demo-canvas";
+            canvas.width = window.screen.availWidth;
+            canvas.height = window.screen.availHeight;
+            /*canvas.style.zIndex = 8;
+            canvas.style.position = "center";
+            canvas.style.border = "1px solid";*/
+
+            positionJoueurX = (canvas.width / 2);
+            positionJoueurY = (canvas.height / 2);
+
             document.getElementsByTagName("body")[0].innerHTML = pageJeuVue;
+            document.body.appendChild(canvas);
+            demoCanvas = document.getElementById("demo-canvas");
 
-            var canvas = document.getElementById("demo-canvas");
-            var ctx = canvas.getContext("2d");
-            initialize();
-
-            stagePrincipal = new createjs.Stage("demo-canvas");
-
-
-
-           /* ctx.canvas.width  = window.screen.availWidth;
-            ctx.canvas.height = window.screen.availHeight;
-            positionJoueurX = (ctx.canvas.width / 2);
-            positionJoueurY = (ctx.canvas.height / 2);*/
-
-
-            console.log("initialiser : canvas width : " + ctx.canvas.width);
-            console.log("initialiser : canvas height : " +  ctx.canvas.height);
+            //var ctx = canvas.getContext("2d");    Peut être utile !! NE PAS SUPPRIMER !!
 
             console.log("initialiser : canvas width : " + canvas.width);
-
-
+            console.log("initialiser : canvas height : " +  canvas.height);
             console.log("initialiser : posX : " + positionJoueurX);
             console.log("initialiser : posY : " + positionJoueurY);
 
+            stagePrincipal = new createjs.Stage("demo-canvas");
             createjs.Ticker.setFPS(60);
             createjs.Ticker.addEventListener("tick", stagePrincipal);
         };
 
-        function initialize() {
-            // Register an event listener to call the resizeCanvas() function
-            // each time the window is resized.
-            window.addEventListener('resize', resizeCanvas, false);
-            // Draw canvas border for the first time.
-            resizeCanvas();
-        }
-
-        function redraw() {
-            ctx.strokeStyle = 'blue';
-            ctx.lineWidth = '5';
-            ctx.strokeRect(0, 0, window.innerWidth, window.innerHeight);
-        }
-
-        function resizeCanvas() {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-            redraw();
-        }
 
         this.afficher = function()
         {
@@ -73,7 +49,7 @@ var JeuVue = (function()
         function afficherCercleJoueur(){
 
             var circle = new createjs.Shape();
-            circle.graphics.beginFill("Black").drawCircle(positionJoueurX, positionJoueurY, 18);
+            circle.graphics.beginFill("Black").drawCircle(0, 0, 18);
             circle.x = positionJoueurX;
             circle.y = positionJoueurY;
             stagePrincipal.addChild(circle);
@@ -83,7 +59,7 @@ var JeuVue = (function()
         function afficherArcDeCercle(){
             var arcDeCercle = new createjs.Shape();
 
-            arcDeCercle.graphics.beginStroke("teal").arc(positionJoueurX, positionJoueurY, 25, 0, Math.PI, true);
+            arcDeCercle.graphics.beginStroke("teal").arc(0, 0, 25, 0, Math.PI, true);
             arcDeCercle.x = positionJoueurX;
             arcDeCercle.y = positionJoueurY;
 
@@ -94,15 +70,14 @@ var JeuVue = (function()
 
             var circle = new createjs.Shape();
 
-            circle.graphics.beginFill("Crimson").drawCircle(positionJoueurX, positionJoueurY, 10);
-            circle.x = positionJoueurX;
-            circle.y = positionJoueurY;
+            circle.graphics.beginFill("Crimson").drawCircle(0, 0, 10);
+            circle.x = 50;
+            circle.y = 50;
 
             stagePrincipal.addChild(circle);
 
             createjs.Tween.get(circle, {loop: true})
-                .to({x:188 , y: 400}, 2000, createjs.Ease.linear);
-
+                .to({x:positionJoueurX - 25, y: positionJoueurY -25}, 2000, createjs.Ease.linear); // 25 = rayon du cercle Joueur
         };
     
 

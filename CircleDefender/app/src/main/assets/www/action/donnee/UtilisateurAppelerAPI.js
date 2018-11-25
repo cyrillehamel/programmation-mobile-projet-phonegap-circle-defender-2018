3e83efea23c1bafc927e891a8470fc09149bad02
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-11-25 14:11:31 -0500

    WIP API

--- CircleDefender/app/src/main/assets/www/action/donnee/UtilisateurAppelerAPI.js
+++ CircleDefender/app/src/main/assets/www/action/donnee/UtilisateurAppelerAPI.js
@@ -43,7 +43,8 @@ var UtilisateurAppelerAPI = function ()
             headers: {
                 'Content-Type': 'application/json'
             }
-        });
+        }
+        );
         var utilisateur = await reponse.json();
         console.log(utilisateur);
 

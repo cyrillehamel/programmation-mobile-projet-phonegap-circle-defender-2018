a944226c63d4a1d2bf88324acb60b4de8471e8e3
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-11-29 16:48:18 -0500

    Récupération des données lors de l'authentification

--- CircleDefender/app/src/main/assets/www/action/application.js
+++ CircleDefender/app/src/main/assets/www/action/application.js
@@ -86,7 +86,7 @@
     
       var actionAuthentifierCompte = function(utilisateur)
     {
-      
+        //appel au DAO
         naviguerAccueil();
     }
 
--- CircleDefender/app/src/main/assets/www/action/vue/AuthentifierVue.js
+++ CircleDefender/app/src/main/assets/www/action/vue/AuthentifierVue.js
@@ -8,18 +8,32 @@ var AuthentifierVue = (function()
         {
             document.getElementsByTagName("body")[0].innerHTML = pageAuthentifierVue;
             var formulaireAuthentifier = document.getElementById("formulaire-authentification");
-            formulaireAuthentifier.addEventListener("submit",AuthentifierUtilisateur);
+            formulaireAuthentifier.addEventListener("submit", authentifierUtilisateur);
  
         }
-        var AuthentifierUtilisateur = function(evenement)
+        var authentifierUtilisateur = function(evenement)
         {
-            
-           
-            actionAuthentifierCompte(Utilsateur);   
+            evenement.preventDefault();
+
+            // Vérif avec des alerts, à réactiver / modifier
+            /*if (document.getElementById("identifiant").value == "") {
+                window.alert("Vous devez entrer un identifiant.");
+                return;
             }
-                
+            if (document.getElementsById("mot_de_passe").value == "") {
+                window.alert("Vous devez entrer un mot de passe.");
+                return;
+            }*/
 
-        
+            var identifiant = document.getElementById("identifiant").value;
+            // Hashage du mot de passe de l'utilisateur :
+            var tableauDeBits = sjcl.hash.sha512.hash(document.getElementById("mot_de_passe").value;);
+            var motDePasse = sjcl.codec.hex.fromBits(tableauDeBits);
+            
+            // création d'un utilisateur
+            var utilisateur = new Utilisateur(null, identifiant, motDePasse, null, null);
 
+            actionAuthentifierCompte(utilisateur);   
+        }
     };
 })();
